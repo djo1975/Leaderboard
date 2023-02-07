195a0831a32393cfabc2e682bb6fc7be6d081ebb
Mladan Ilic <mladanilic1975@gmail.com>
2023-02-07 20:30:02 +0100

    to live empty form after submit

--- src/index.js
+++ src/index.js
@@ -11,3 +11,16 @@ document.getElementById('add-form').addEventListener('submit', (event) => {
   const score = document.getElementById('score').value;
   addScore(user, score);
 });
+
+function clearInputs() {
+  document.getElementById('name').value = '';
+  document.getElementById('score').value = '';
+}
+
+document.getElementById('add-form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  const user = document.getElementById('name').value;
+  const score = document.getElementById('score').value;
+  addScore(user, score);
+  clearInputs();
+});
